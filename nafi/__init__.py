a74b368071ee578954921e4c66f40c029f098b0b
Jelle Aalbers <jelleaalbers@gmail.com>
2023-10-04 15:10:01 +0200

    Simplify imports for IDEs

--- nafi/__init__.py
+++ nafi/__init__.py
@@ -1,19 +1,10 @@
-import importlib
-
 __version__ = '0.0.0'
-__all__ = []
 
-
-submodules = ['utils', 'frequentist', 'bayes', 'interval_finding', 'performance']
-
-for submodule in submodules:
-    # Import submodule
-    submod = importlib.import_module(f'.{submodule}', package=__name__)
-    # Import everything from __all__ in from submodule
-    for x in submod.__all__:
-        globals()[x] = getattr(submod, x)
-    # Add everything from submodule to __all__
-    __all__ += submod.__all__
+from .utils import *
+from .frequentist import *
+from .bayes import *
+from .interval_finding import *
+from .performance import *
 
 from . import likelihoods
 from . import high_level
--- nafi/high_level/__init__.py
+++ nafi/high_level/__init__.py
@@ -1,13 +1,8 @@
-import importlib
+from . import experiments
+from .experiments import *
 
-__all__ = []
-submodules = ['experiments', 'methods', 'plots']
+from . import methods
+from .methods import *
 
-for submodule in submodules:
-    # Import submodule
-    submod = importlib.import_module(f'.{submodule}', package=__name__)
-    # Import everything from __all__ in from submodule
-    for x in submod.__all__:
-        globals()[x] = getattr(submod, x)
-    # Add everything from submodule to __all__
-    __all__ += submod.__all__
+from . import plots
+from .plots import *
